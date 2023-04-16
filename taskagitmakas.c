19ac6f82112a26f502c0aa1354006cb12133e79b
kubilayguler <glr.kubilay45@gmail.com>
2023-04-16 22:15:13 +0300

    Update taskagitmakas.c

--- taskagitmakas.c
+++ taskagitmakas.c
@@ -5,7 +5,9 @@
 int main(){
 	srand(time(NULL));
 	int oyuncu,Bot;
+	char ch;
 	
+	tekrar:
 	printf("TAS-KAGIT-MAKAS OYUNU\n");
 	printf("**************************");
 	printf("\n1 => TAS\n");
@@ -16,22 +18,26 @@ int main(){
 	secim:	
 	printf("1,2,3'ten birini yazmalisin: ");
 	scanf("%d",&oyuncu);
-	if(oyuncu==1 || oyuncu==2 || oyuncu==3){
+	if(oyuncu==1 || oyuncu==2 || oyuncu==3)
+	{
 		Bot = rand()%3+1;
 		//Oyuncu:Tas Bot:Tas
-		if(oyuncu==1 && Bot==1){
+		if(oyuncu==1 && Bot==1)
+		{
 			system("color 0E");
 			printf("\nOyuncu => Tas | Tas <= BOT \n");
 			printf("\nSonuc: BERABERE !.. ");
 		}
 		//Oyuncu:Tas Bot:Kagit
-		if(oyuncu==1 && Bot==2){
+		else if(oyuncu==1 && Bot==2)
+		{
 			system("color 0C");
 			printf("\nOyuncu => Tas | Kagit <= BOT \n");
 			printf("\nSonuc: KAYBETTIN !.. ");
 		}
 		//Oyuncu:Tas Bot:Makas
-		if(oyuncu==1 && Bot==3){
+		else if(oyuncu==1 && Bot==3)
+		{
 			system("color 0a");
 			printf("\nOyuncu => Tas | Makas <= BOT \n");
 			printf("\nSonuc: KAZANDIN !.. ");
@@ -39,19 +45,22 @@ int main(){
 		
 		
 		//Oyuncu:Kagit Bot:Tas
-		if(oyuncu==2 && Bot==1){
+		if(oyuncu==2 && Bot==1)
+		{
 			system("color 0A");
 			printf("\nOyuncu => Kagit | Tas <= BOT \n");
 			printf("\nSonuc: KAZANDIN !.. ");
 		}
 		//Oyuncu:Kagit Bot:Kagit
-		if(oyuncu==2 && Bot==2){
+		else if(oyuncu==2 && Bot==2)
+		{
 			system("color 0E");
 			printf("\nOyuncu => Kagit | Kagit <= BOT \n");
 			printf("\nSonuc: BERABERE !.. ");
 		}
 		//Oyuncu:Kagit Bot:Makas
-		if(oyuncu==2 && Bot==3){
+		else if(oyuncu==2 && Bot==3)
+		{
 			system("color 0C");
 			printf("\nOyuncu => Kagit | Makas <= BOT \n");
 			printf("\nSonuc: KAYBETTIN !.. ");
@@ -59,28 +68,46 @@ int main(){
 		
 		
 		//Oyuncu:Makas Bot:Tas
-		if(oyuncu==3 && Bot==1){
+		if(oyuncu==3 && Bot==1)
+		{
 			system("color 0C");
 			printf("\nOyuncu => Makas | Tas <= BOT \n");
 			printf("\nSonuc: KAYBETTIN !.. ");
 		}
 		//Oyuncu:Kagit Bot:Kagit
-		if(oyuncu==3 && Bot==2){
+		else if(oyuncu==3 && Bot==2)
+		{
 			system("color 0A");
 			printf("\nOyuncu => Makas | Kagit <= BOT \n");
 			printf("\nSonuc: KAZANDIN !.. ");
 		}
 		//Oyuncu:Kagit Bot:Makas
-		if(oyuncu==3 && Bot==3){
+		else if(oyuncu==3 && Bot==3)
+		{
 			system("color 0E");
 			printf("\nOyuncu => Makas | Makas <= BOT \n");
 			printf("\nSonuc: BERABERE !.. ");
 		}
-			
 	}
-	else{
-		goto secim;
-	}
+		else
+		{
+			goto secim;
+		}
 	
+		printf("Tekrar Oynamak ister misin?(E/H) : ");
+		soru:
+		scanf("%c",&ch);
+		if(ch=='E' || ch=='e'){
+			system("cls");
+			system("color 0F");
+			goto tekrar;
+		}
+		else if(ch=='H' || ch=='h'){
+			exit(0);
+		}
+		else{
+			goto soru;
+		}
+		
 	return 0;
 }
