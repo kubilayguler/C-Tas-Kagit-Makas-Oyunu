d5152d2dce93514a494c43bc3ab15f42c4432f73
kubilayguler <glr.kubilay45@gmail.com>
2023-04-16 17:20:18 +0300

    Tas kagit makas oyunu

---
+++ taskagitmakas.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+	srand(time(NULL));
+	int oyuncu,Bot;
+	
+	printf("TAS-KAGIT-MAKAS OYUNU\n");
+	printf("**************************");
+	printf("\n1 => TAS\n");
+	printf("\n2 => KAGIT\n");
+	printf("\n3 => MAKAS\n");
+	printf("**************************\n\n");
+	
+	secim:	
+	printf("1,2,3'ten birini yazmalisin: ");
+	scanf("%d",&oyuncu);
+	if(oyuncu==1 || oyuncu==2 || oyuncu==3){
+		Bot = rand()%3+1;
+		//Oyuncu:Tas Bot:Tas
+		if(oyuncu==1 && Bot==1){
+			system("color 0E");
+			printf("\nOyuncu => Tas | Tas <= BOT \n");
+			printf("\nSonuc: BERABERE !.. ");
+		}
+		//Oyuncu:Tas Bot:Kagit
+		if(oyuncu==1 && Bot==2){
+			system("color 0C");
+			printf("\nOyuncu => Tas | Kagit <= BOT \n");
+			printf("\nSonuc: KAYBETTIN !.. ");
+		}
+		//Oyuncu:Tas Bot:Makas
+		if(oyuncu==1 && Bot==3){
+			system("color 0a");
+			printf("\nOyuncu => Tas | Makas <= BOT \n");
+			printf("\nSonuc: KAZANDIN !.. ");
+		}
+		
+		
+		//Oyuncu:Kagit Bot:Tas
+		if(oyuncu==2 && Bot==1){
+			system("color 0A");
+			printf("\nOyuncu => Kagit | Tas <= BOT \n");
+			printf("\nSonuc: KAZANDIN !.. ");
+		}
+		//Oyuncu:Kagit Bot:Kagit
+		if(oyuncu==2 && Bot==2){
+			system("color 0E");
+			printf("\nOyuncu => Kagit | Kagit <= BOT \n");
+			printf("\nSonuc: BERABERE !.. ");
+		}
+		//Oyuncu:Kagit Bot:Makas
+		if(oyuncu==2 && Bot==3){
+			system("color 0C");
+			printf("\nOyuncu => Kagit | Makas <= BOT \n");
+			printf("\nSonuc: KAYBETTIN !.. ");
+		}
+		
+		
+		//Oyuncu:Makas Bot:Tas
+		if(oyuncu==3 && Bot==1){
+			system("color 0C");
+			printf("\nOyuncu => Makas | Tas <= BOT \n");
+			printf("\nSonuc: KAYBETTIN !.. ");
+		}
+		//Oyuncu:Kagit Bot:Kagit
+		if(oyuncu==3 && Bot==2){
+			system("color 0A");
+			printf("\nOyuncu => Makas | Kagit <= BOT \n");
+			printf("\nSonuc: KAZANDIN !.. ");
+		}
+		//Oyuncu:Kagit Bot:Makas
+		if(oyuncu==3 && Bot==3){
+			system("color 0E");
+			printf("\nOyuncu => Makas | Makas <= BOT \n");
+			printf("\nSonuc: BERABERE !.. ");
+		}
+			
+	}
+	else{
+		goto secim;
+	}
+	
+	return 0;
+}
